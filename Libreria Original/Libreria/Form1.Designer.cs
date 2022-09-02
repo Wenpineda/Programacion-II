df062c5923c33c9bd7e114375a0265a7d22218ee
Wenpineda <cp0428032021@unab.edu.sv>
2022-09-02 15:03:12 -0600

    Lista-Libreria

---
+++ Libreria Original/Libreria/Form1.Designer.cs
@@ -0,0 +1,375 @@
+﻿
+namespace Libreria
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txttitulo = new System.Windows.Forms.TextBox();
+            this.cbgenero = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtautor = new System.Windows.Forms.TextBox();
+            this.txtyear = new System.Windows.Forms.TextBox();
+            this.txtedicion = new System.Windows.Forms.TextBox();
+            this.txtpais = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnmodificar = new System.Windows.Forms.Button();
+            this.btnguardar = new System.Windows.Forms.Button();
+            this.btneliminar = new System.Windows.Forms.Button();
+            this.btnregistrar = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtbuscar = new System.Windows.Forms.TextBox();
+            this.btnbuscar = new System.Windows.Forms.Button();
+            this.rthRegistro = new System.Windows.Forms.RichTextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+            this.panel1.Location = new System.Drawing.Point(1, 2);
+            this.panel1.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(727, 152);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Location = new System.Drawing.Point(35, 38);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Titulo";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txttitulo
+            // 
+            this.txttitulo.Location = new System.Drawing.Point(124, 31);
+            this.txttitulo.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.txttitulo.Name = "txttitulo";
+            this.txttitulo.Size = new System.Drawing.Size(207, 26);
+            this.txttitulo.TabIndex = 2;
+            // 
+            // cbgenero
+            // 
+            this.cbgenero.FormattingEnabled = true;
+            this.cbgenero.Items.AddRange(new object[] {
+            "Terror",
+            "Misterio",
+            "Romance",
+            "Comedia",
+            "Ciencia Ficcion",
+            "Programacion",
+            "Educacion",
+            "Seguridad"});
+            this.cbgenero.Location = new System.Drawing.Point(124, 121);
+            this.cbgenero.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.cbgenero.Name = "cbgenero";
+            this.cbgenero.Size = new System.Drawing.Size(207, 26);
+            this.cbgenero.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Location = new System.Drawing.Point(35, 81);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Edicion";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Location = new System.Drawing.Point(35, 245);
+            this.label3.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Año";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label4.Location = new System.Drawing.Point(34, 162);
+            this.label4.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Autor";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label5.Location = new System.Drawing.Point(35, 121);
+            this.label5.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(70, 20);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Genero";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label6.Location = new System.Drawing.Point(35, 206);
+            this.label6.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(44, 20);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Pais";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Gray;
+            this.groupBox1.Controls.Add(this.txtautor);
+            this.groupBox1.Controls.Add(this.cbgenero);
+            this.groupBox1.Controls.Add(this.txtyear);
+            this.groupBox1.Controls.Add(this.txtedicion);
+            this.groupBox1.Controls.Add(this.txtpais);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.txttitulo);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Location = new System.Drawing.Point(13, 162);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(402, 294);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Datos de Registro de Libros";
+            // 
+            // txtautor
+            // 
+            this.txtautor.Location = new System.Drawing.Point(124, 166);
+            this.txtautor.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.txtautor.Name = "txtautor";
+            this.txtautor.Size = new System.Drawing.Size(207, 26);
+            this.txtautor.TabIndex = 13;
+            // 
+            // txtyear
+            // 
+            this.txtyear.Location = new System.Drawing.Point(124, 249);
+            this.txtyear.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.txtyear.Name = "txtyear";
+            this.txtyear.Size = new System.Drawing.Size(207, 26);
+            this.txtyear.TabIndex = 12;
+            // 
+            // txtedicion
+            // 
+            this.txtedicion.Location = new System.Drawing.Point(124, 75);
+            this.txtedicion.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.txtedicion.Name = "txtedicion";
+            this.txtedicion.Size = new System.Drawing.Size(207, 26);
+            this.txtedicion.TabIndex = 11;
+            // 
+            // txtpais
+            // 
+            this.txtpais.Location = new System.Drawing.Point(124, 206);
+            this.txtpais.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.txtpais.Name = "txtpais";
+            this.txtpais.Size = new System.Drawing.Size(207, 26);
+            this.txtpais.TabIndex = 10;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.Gray;
+            this.groupBox2.Controls.Add(this.btnmodificar);
+            this.groupBox2.Controls.Add(this.btnguardar);
+            this.groupBox2.Controls.Add(this.btneliminar);
+            this.groupBox2.Controls.Add(this.btnregistrar);
+            this.groupBox2.Location = new System.Drawing.Point(471, 162);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(189, 294);
+            this.groupBox2.TabIndex = 10;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Gestion de Registros";
+            // 
+            // btnmodificar
+            // 
+            this.btnmodificar.BackColor = System.Drawing.Color.PaleGreen;
+            this.btnmodificar.Location = new System.Drawing.Point(30, 87);
+            this.btnmodificar.Name = "btnmodificar";
+            this.btnmodificar.Size = new System.Drawing.Size(134, 39);
+            this.btnmodificar.TabIndex = 14;
+            this.btnmodificar.Text = "Modificar";
+            this.btnmodificar.UseVisualStyleBackColor = false;
+            this.btnmodificar.Click += new System.EventHandler(this.btnmodificar_Click);
+            // 
+            // btnguardar
+            // 
+            this.btnguardar.BackColor = System.Drawing.Color.PaleGreen;
+            this.btnguardar.Enabled = false;
+            this.btnguardar.Location = new System.Drawing.Point(30, 151);
+            this.btnguardar.Name = "btnguardar";
+            this.btnguardar.Size = new System.Drawing.Size(134, 41);
+            this.btnguardar.TabIndex = 13;
+            this.btnguardar.Text = "Guardar";
+            this.btnguardar.UseVisualStyleBackColor = false;
+            this.btnguardar.Click += new System.EventHandler(this.btnguardar_Click);
+            // 
+            // btneliminar
+            // 
+            this.btneliminar.BackColor = System.Drawing.Color.Red;
+            this.btneliminar.Location = new System.Drawing.Point(30, 234);
+            this.btneliminar.Name = "btneliminar";
+            this.btneliminar.Size = new System.Drawing.Size(134, 41);
+            this.btneliminar.TabIndex = 12;
+            this.btneliminar.Text = "Eliminar";
+            this.btneliminar.UseVisualStyleBackColor = false;
+            this.btneliminar.Click += new System.EventHandler(this.btneliminar_Click);
+            // 
+            // btnregistrar
+            // 
+            this.btnregistrar.BackColor = System.Drawing.Color.PaleGreen;
+            this.btnregistrar.Location = new System.Drawing.Point(30, 31);
+            this.btnregistrar.Name = "btnregistrar";
+            this.btnregistrar.Size = new System.Drawing.Size(134, 39);
+            this.btnregistrar.TabIndex = 11;
+            this.btnregistrar.Text = "Registrar";
+            this.btnregistrar.UseVisualStyleBackColor = false;
+            this.btnregistrar.Click += new System.EventHandler(this.btnregistrar_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label7.Location = new System.Drawing.Point(47, 484);
+            this.label7.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(54, 20);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Titulo";
+            // 
+            // txtbuscar
+            // 
+            this.txtbuscar.Location = new System.Drawing.Point(137, 481);
+            this.txtbuscar.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.txtbuscar.Name = "txtbuscar";
+            this.txtbuscar.Size = new System.Drawing.Size(207, 26);
+            this.txtbuscar.TabIndex = 14;
+            // 
+            // btnbuscar
+            // 
+            this.btnbuscar.BackColor = System.Drawing.Color.PaleGreen;
+            this.btnbuscar.Location = new System.Drawing.Point(501, 473);
+            this.btnbuscar.Name = "btnbuscar";
+            this.btnbuscar.Size = new System.Drawing.Size(134, 41);
+            this.btnbuscar.TabIndex = 15;
+            this.btnbuscar.Text = "Buscar";
+            this.btnbuscar.UseVisualStyleBackColor = false;
+            this.btnbuscar.Click += new System.EventHandler(this.btnbuscar_Click);
+            // 
+            // rthRegistro
+            // 
+            this.rthRegistro.Location = new System.Drawing.Point(13, 533);
+            this.rthRegistro.Name = "rthRegistro";
+            this.rthRegistro.Size = new System.Drawing.Size(647, 96);
+            this.rthRegistro.TabIndex = 16;
+            this.rthRegistro.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(674, 659);
+            this.Controls.Add(this.rthRegistro);
+            this.Controls.Add(this.btnbuscar);
+            this.Controls.Add(this.txtbuscar);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txttitulo;
+        private System.Windows.Forms.ComboBox cbgenero;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtautor;
+        private System.Windows.Forms.TextBox txtyear;
+        private System.Windows.Forms.TextBox txtedicion;
+        private System.Windows.Forms.TextBox txtpais;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnmodificar;
+        private System.Windows.Forms.Button btnguardar;
+        private System.Windows.Forms.Button btneliminar;
+        private System.Windows.Forms.Button btnregistrar;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtbuscar;
+        private System.Windows.Forms.Button btnbuscar;
+        private System.Windows.Forms.RichTextBox rthRegistro;
+    }
+}
+
---
+++ Libreria Original/Libreria/Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Libreria
+{
+    struct Libros
+    {
+        public string titulo;
+        public string edicion;
+        public string genero;
+        public string autor;
+        public string pais;
+        public int year;
+    }
+    public partial class Form1 : Form
+    {
+        //Declaracion del arreglo tipo estructura
+        Libros[] Lib = new Libros[100];
+        //variable de uso global
+        int indice = 0, iModificar;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //metodo para limpiar los campos de texto 
+        void LimpiarCampos()
+        {
+            //Clear(); Funciona para limpiar los campos tambien se puede poner el "";
+            txttitulo.Text = "";
+            txtedicion.Text = "";
+            cbgenero.Text = "";
+            txtautor.Text = "";
+            txtpais.Text = "";
+            txtyear.Text = "";
+        }
+        void Registrar()
+        {
+            try
+            {
+                if (txttitulo.Text !=""&& txtedicion.Text !=""&& cbgenero.Text!="")
+                {
+                    Lib[indice].titulo = txttitulo.Text;
+                    Lib[indice].edicion = txtedicion.Text;
+                    Lib[indice].genero = Convert.ToString(cbgenero.Text);
+                    Lib[indice].autor = txtautor.Text;
+                    Lib[indice].pais = txtpais.Text;
+                    Lib[indice].year = Convert.ToInt32( txtyear.Text);
+                    indice++;
+                }
+            }
+            catch (Exception e)
+            {
+
+                MessageBox.Show("Error en el registro del libro", e.Message);
+            }
+        }
+        //Metodo para mostrar los datos
+        void MostrarRegistros()
+        {
+            try
+            {
+                string Lista = "";
+                for (int i = 0; i < indice; i++)
+                {
+                    Lista = Lista + i + " - " + Lib[i].titulo + " " + Lib[i].edicion + " " + Lib[i].genero + " " + Lib[i].autor + " " + Lib[i].pais + " " + Lib[i].year + "\n";
+                }
+                rthRegistro.Text = Lista;
+            }
+            catch (Exception e)
+            {
+
+                MessageBox.Show("Ocurrio un error a la hora de cargar los datos", e.Message);
+            }
+        }
+        void BuscarRegistro()
+        {
+            try
+            {
+                if (txtbuscar.Text !="")
+                {
+                    for (int i = 0; i < indice; i++)
+                    {
+                        if (Lib[i].titulo == txtbuscar.Text)
+                        {
+                            txttitulo.Text = Lib[i].titulo;
+                            txtedicion.Text = Lib[i].edicion;
+                            cbgenero.SelectedText = Convert.ToString(Lib[i].genero);
+                            txtautor.Text = Lib[i].autor;
+                            txtpais.Text = Lib[i].pais;
+                            txtyear.Text = Convert.ToString(Lib[i].year);
+                            iModificar = i;
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("NO se encontro nada " + e.Message); ;
+            }
+        }
+
+        public void GuardarCambios()
+        {
+            try
+            {
+                // MessageBox.Show("Indice" + iModificar); "Verificar que si esta llamando la variable
+
+                if (txttitulo.Text != "" && txtedicion.Text != "" && cbgenero.Text != "")
+                {
+                    Lib[iModificar].titulo = txttitulo.Text;
+                    Lib[iModificar].edicion = txtedicion.Text;
+                    Lib[iModificar].genero =Convert.ToString(cbgenero.Text);
+                    Lib[iModificar].autor = txtautor.Text;
+                    Lib[iModificar].pais = txtpais.Text;
+                    Lib[iModificar].year = Convert.ToInt32(txtyear.Text);
+                    
+                }
+
+            }
+            catch (Exception e)
+            {
+
+                MessageBox.Show("Error al guardar los cambions generados");
+            }
+        }
+        public void EliminarRegistro()
+        {
+            try
+            {
+                for (int i = iModificar; i <indice; i++)
+                {
+                    Lib[i].titulo = Lib[i + 1].titulo; 
+                    Lib[i].edicion = Lib[i + 1].edicion;
+                    Lib[i].genero = Lib[i + 1].genero;
+                    Lib[i].autor = Lib[i + 1].autor;
+                    Lib[i].pais = Lib[i + 1].pais;
+                    Lib[i].year = Lib[i + 1].year;
+                }
+                indice--;
+            }
+            catch (Exception e)
+            {
+
+                MessageBox.Show("Error al eliminar el registro");
+            }
+        }
+
+    private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnmodificar_Click(object sender, EventArgs e)
+        {
+            btnguardar.Enabled = true;
+            btnmodificar.Enabled = false;
+        }
+
+        private void btnguardar_Click(object sender, EventArgs e)
+        {
+            btnmodificar.Enabled = true;
+            btnguardar.Enabled = false;
+            GuardarCambios();
+            MostrarRegistros();
+        }
+
+        private void btnbuscar_Click(object sender, EventArgs e)
+        {
+            LimpiarCampos();
+            BuscarRegistro();
+        }
+
+        private void btneliminar_Click(object sender, EventArgs e)
+        {
+            EliminarRegistro();
+            MostrarRegistros();
+            LimpiarCampos();
+        }
+
+        private void btnregistrar_Click(object sender, EventArgs e)
+        {
+            Registrar();
+            MostrarRegistros();
+            LimpiarCampos();
+        }
+    }
+}
