680cd0bf12aa1e0328ed845c29b8817b3a0e6ddc
Wenpineda <cp0428032021@unab.edu.sv>
2022-09-26 12:06:58 -0600

    Registro de Notas

---
+++ RegistroDeNotas/RegistroDeNotas/Form1.Designer.cs
@@ -0,0 +1,316 @@
+﻿namespace RegistroDeNotas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textCodigo = new System.Windows.Forms.TextBox();
+            this.textParcial = new System.Windows.Forms.TextBox();
+            this.textLaboratorio = new System.Windows.Forms.TextBox();
+            this.textAsignatura = new System.Windows.Forms.TextBox();
+            this.textApellido = new System.Windows.Forms.TextBox();
+            this.textNombre = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnRegistrar = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.rtbRegistros = new System.Windows.Forms.RichTextBox();
+            this.textBuscar = new System.Windows.Forms.TextBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Aquamarine;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(20, 320);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Codigo ";
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Aquamarine;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(20, 184);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 26);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Laboratorio ";
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Aquamarine;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(20, 58);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 26);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Asignatura";
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.Aquamarine;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(20, 142);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(92, 26);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Apellido";
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.Aquamarine;
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(20, 98);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(92, 26);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Nombre";
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.Color.Aquamarine;
+            this.label6.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(20, 231);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(92, 26);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Parcial";
+            // 
+            // textCodigo
+            // 
+            this.textCodigo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textCodigo.Location = new System.Drawing.Point(155, 318);
+            this.textCodigo.Name = "textCodigo";
+            this.textCodigo.Size = new System.Drawing.Size(94, 26);
+            this.textCodigo.TabIndex = 6;
+            // 
+            // textParcial
+            // 
+            this.textParcial.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textParcial.Location = new System.Drawing.Point(155, 231);
+            this.textParcial.Name = "textParcial";
+            this.textParcial.Size = new System.Drawing.Size(94, 26);
+            this.textParcial.TabIndex = 7;
+            // 
+            // textLaboratorio
+            // 
+            this.textLaboratorio.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textLaboratorio.Location = new System.Drawing.Point(155, 184);
+            this.textLaboratorio.Name = "textLaboratorio";
+            this.textLaboratorio.Size = new System.Drawing.Size(94, 26);
+            this.textLaboratorio.TabIndex = 8;
+            // 
+            // textAsignatura
+            // 
+            this.textAsignatura.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textAsignatura.Location = new System.Drawing.Point(155, 58);
+            this.textAsignatura.Name = "textAsignatura";
+            this.textAsignatura.Size = new System.Drawing.Size(163, 26);
+            this.textAsignatura.TabIndex = 9;
+            // 
+            // textApellido
+            // 
+            this.textApellido.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textApellido.Location = new System.Drawing.Point(155, 142);
+            this.textApellido.Name = "textApellido";
+            this.textApellido.Size = new System.Drawing.Size(163, 26);
+            this.textApellido.TabIndex = 10;
+            // 
+            // textNombre
+            // 
+            this.textNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textNombre.Location = new System.Drawing.Point(155, 98);
+            this.textNombre.Name = "textNombre";
+            this.textNombre.Size = new System.Drawing.Size(163, 26);
+            this.textNombre.TabIndex = 11;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(155, 273);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(94, 26);
+            this.textBox1.TabIndex = 12;
+            // 
+            // label7
+            // 
+            this.label7.BackColor = System.Drawing.Color.Aquamarine;
+            this.label7.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(20, 273);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(92, 26);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Promedio";
+            // 
+            // btnRegistrar
+            // 
+            this.btnRegistrar.BackColor = System.Drawing.Color.SeaGreen;
+            this.btnRegistrar.ForeColor = System.Drawing.Color.White;
+            this.btnRegistrar.Location = new System.Drawing.Point(497, 387);
+            this.btnRegistrar.Name = "btnRegistrar";
+            this.btnRegistrar.Size = new System.Drawing.Size(100, 39);
+            this.btnRegistrar.TabIndex = 14;
+            this.btnRegistrar.Text = "Registar";
+            this.btnRegistrar.UseVisualStyleBackColor = false;
+            this.btnRegistrar.Click += new System.EventHandler(this.btnRegistrar_Click);
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.BackColor = System.Drawing.Color.SeaGreen;
+            this.btnGuardar.ForeColor = System.Drawing.Color.White;
+            this.btnGuardar.Location = new System.Drawing.Point(171, 387);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(100, 39);
+            this.btnGuardar.TabIndex = 15;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = false;
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.BackColor = System.Drawing.Color.SeaGreen;
+            this.btnEditar.ForeColor = System.Drawing.Color.White;
+            this.btnEditar.Location = new System.Drawing.Point(348, 387);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(100, 39);
+            this.btnEditar.TabIndex = 16;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = false;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.BackColor = System.Drawing.Color.SeaGreen;
+            this.btnEliminar.ForeColor = System.Drawing.Color.White;
+            this.btnEliminar.Location = new System.Drawing.Point(12, 387);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(100, 39);
+            this.btnEliminar.TabIndex = 17;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = false;
+            // 
+            // rtbRegistros
+            // 
+            this.rtbRegistros.Location = new System.Drawing.Point(12, 446);
+            this.rtbRegistros.Name = "rtbRegistros";
+            this.rtbRegistros.Size = new System.Drawing.Size(585, 155);
+            this.rtbRegistros.TabIndex = 18;
+            this.rtbRegistros.Text = "";
+            // 
+            // textBuscar
+            // 
+            this.textBuscar.Location = new System.Drawing.Point(55, 13);
+            this.textBuscar.Name = "textBuscar";
+            this.textBuscar.Size = new System.Drawing.Size(285, 20);
+            this.textBuscar.TabIndex = 19;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(373, 13);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(75, 23);
+            this.btnBuscar.TabIndex = 20;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.ClientSize = new System.Drawing.Size(609, 613);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.textBuscar);
+            this.Controls.Add(this.rtbRegistros);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnEditar);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.btnRegistrar);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textNombre);
+            this.Controls.Add(this.textApellido);
+            this.Controls.Add(this.textAsignatura);
+            this.Controls.Add(this.textLaboratorio);
+            this.Controls.Add(this.textParcial);
+            this.Controls.Add(this.textCodigo);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Registro de Notas";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textCodigo;
+        private System.Windows.Forms.TextBox textParcial;
+        private System.Windows.Forms.TextBox textLaboratorio;
+        private System.Windows.Forms.TextBox textAsignatura;
+        private System.Windows.Forms.TextBox textApellido;
+        private System.Windows.Forms.TextBox textNombre;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btnRegistrar;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.RichTextBox rtbRegistros;
+        private System.Windows.Forms.TextBox textBuscar;
+        private System.Windows.Forms.Button btnBuscar;
+    }
+}
+
---
+++ RegistroDeNotas/RegistroDeNotas/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace RegistroDeNotas
+{
+    public partial class Form1 : Form
+    {
+        ArrayList RegistroNotas = new ArrayList();
+        int indice, numcod=0;
+        string codigo;
+        double promedio;
+        void LimpiarObjetos()
+        {
+            textNombre.Clear();
+            textApellido.Clear();
+            textAsignatura.Clear();
+            textLaboratorio.Clear();
+            textParcial.Clear();    
+        }
+       
+        void RegistrarNotas()
+        {
+            codigo = "ST-00" + numcod;
+            numcod++;
+            RegistroNotas.Add(codigo);
+            RegistroNotas.Add(textNombre.Text);
+            RegistroNotas.Add(textApellido.Text);
+            RegistroNotas.Add(textAsignatura.Text);
+            RegistroNotas.Add(textLaboratorio.Text);
+            RegistroNotas.Add(textParcial.Text);
+            promedio=((Convert.ToDouble(textLaboratorio.Text)+Convert.ToDouble(textParcial.Text))/2);
+            RegistroNotas.Add(promedio);
+
+        }
+        void MostrarRegistros()
+        {
+            string Regdatos = "";
+            for (int i = 0; i < RegistroNotas.Count; i += 7) 
+            {
+                Regdatos = Regdatos +RegistroNotas[i] +" "+ RegistroNotas[i+1]+" " + RegistroNotas[i+2]+" " + RegistroNotas[i+3]+" " + RegistroNotas[i+4]+" " + RegistroNotas[i+5]+" " + RegistroNotas[i+6] + " \n";
+            }
+            rtbRegistros.Text= Regdatos;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRegistrar_Click(object sender, EventArgs e)
+        {
+            RegistrarNotas();
+            MostrarRegistros();
+            LimpiarObjetos();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
