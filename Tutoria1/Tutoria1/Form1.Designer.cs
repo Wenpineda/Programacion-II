f197f587008ae13ada454c3d887e8fff18147c49
Wenpineda <cp0428032021@unab.edu.sv>
2022-09-26 12:52:48 -0600

    Tutoria#1 basico

---
+++ Tutoria1/Tutoria1/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace Tutoria1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.textNombre = new System.Windows.Forms.TextBox();
+            this.lblista = new System.Windows.Forms.Label();
+            this.textIndice = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(324, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 1;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(233, 292);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 2;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Location = new System.Drawing.Point(375, 291);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(75, 23);
+            this.btnEditar.TabIndex = 3;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = true;
+            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(541, 291);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 4;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // textNombre
+            // 
+            this.textNombre.Location = new System.Drawing.Point(233, 265);
+            this.textNombre.Name = "textNombre";
+            this.textNombre.Size = new System.Drawing.Size(418, 20);
+            this.textNombre.TabIndex = 5;
+            this.textNombre.TextChanged += new System.EventHandler(this.textNombre_TextChanged);
+            // 
+            // lblista
+            // 
+            this.lblista.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblista.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblista.Location = new System.Drawing.Point(115, 21);
+            this.lblista.Name = "lblista";
+            this.lblista.Size = new System.Drawing.Size(570, 210);
+            this.lblista.TabIndex = 6;
+            this.lblista.Click += new System.EventHandler(this.lblista_Click);
+            // 
+            // textIndice
+            // 
+            this.textIndice.Location = new System.Drawing.Point(532, 234);
+            this.textIndice.Name = "textIndice";
+            this.textIndice.Size = new System.Drawing.Size(119, 20);
+            this.textIndice.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(479, 241);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Indice";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textIndice);
+            this.Controls.Add(this.lblista);
+            this.Controls.Add(this.textNombre);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnEditar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.label2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.TextBox textNombre;
+        private System.Windows.Forms.Label lblista;
+        private System.Windows.Forms.TextBox textIndice;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Tutoria1/Tutoria1/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tutoria1
+{
+    public partial class Form1 : Form
+    {
+       // string[] arrayEstudiantes = new string[5];
+       //ArrayList ListaNombres = new ArrayList();
+        string nombre = "";
+        //poner tipo de dato
+        // En la lista compras no se sobrescriben los datos ingresados solo se acumulan, pero asdasd va primero
+        List<string> compras= new List<string>() { "asdasd", "asdash"};
+        List<int> numeros = new List<int>() { 123, 456, 786, 987 };
+        public Form1()
+        {
+            InitializeComponent();
+            compras.Add("atun");
+            compras.Add("Pan caja");
+            compras.Add("salsa");
+            //Formas de recorrer una lista
+            //1-Recorrer lista con un foreach
+            //foreach (var item in compras)
+            //{
+            //    lblista.Text += item + "\n";
+            //}
+            //2-Recorrer lista con list.ForEach
+            compras.ForEach(i => lblista.Text += i + "\n");
+
+            /* Lo de la clase pasada
+             ListaNombres.Add("Sancho Panza");
+            ListaNombres.Add(true);
+            ListaNombres.Add(99);
+            {
+
+                renderList();
+
+            }*/
+        }
+
+        private void textNombre_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            //string nombre = textNombre.Text;
+            //ListaNombres.Add(nombre);
+            ////Console.Write(ListaNombres)
+            //renderList();
+
+        }
+        public void renderList()    
+        {
+            //lblista.Text = "";
+            //for (int i = 0; i < ListaNombres.Count; i++)
+            //{
+            //    lblista.Text += i + "-" + ListaNombres[i].ToString() + "\n";
+
+
+            //}
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            //string indice = textIndice.Text;
+            //int posision = Int32.Parse(indice);
+            //ListaNombres[posision] = textNombre.Text;
+            //renderList();
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            //string indice = textIndice.Text;
+            //int posicion=Int32.Parse(indice);
+            //ListaNombres.RemoveAt(posicion);
+            //renderList();
+        }
+
+        private void lblista_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+       
+}
